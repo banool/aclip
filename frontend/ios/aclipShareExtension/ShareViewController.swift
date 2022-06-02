56d4b1cff7329ca297f374629ead0aa9abb9bb46
Daniel Porteous <danielporteous1@gmail.com>
2022-06-01 18:46:42 -0700

    Add basic framework for sharing to aclip

---
+++ frontend/ios/aclipShareExtension/ShareViewController.swift
@@ -0,0 +1,126 @@
+import UIKit
+import Social
+import MobileCoreServices
+import Photos
+
+class ShareViewController: SLComposeServiceViewController {
+    var hostAppBundleIdentifier = ""
+    var appGroupId = ""
+    let sharedKey = "ShareKey"
+    var sharedText: [String] = []
+    let urlContentType = kUTTypeURL as String
+
+    override func isContentValid() -> Bool {
+        return true
+    }
+
+    private func loadIds() {
+        // loading Share extension App Id
+        let shareExtensionAppBundleIdentifier = Bundle.main.bundleIdentifier!;
+
+
+        // convert ShareExtension id to host app id
+        // By default it is remove last part of id after last point
+        // For example: com.test.ShareExtension -> com.test
+        let lastIndexOfPoint = shareExtensionAppBundleIdentifier.lastIndex(of: ".");
+        hostAppBundleIdentifier = String(shareExtensionAppBundleIdentifier[..<lastIndexOfPoint!]);
+
+        // loading custom AppGroupId from Build Settings or use group.<hostAppBundleIdentifier>
+        appGroupId = (Bundle.main.object(forInfoDictionaryKey: "AppGroupId") as? String) ?? "group.\(hostAppBundleIdentifier)";
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+
+        // load group and app id from build info
+        loadIds();
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+        if let content = extensionContext!.inputItems[0] as? NSExtensionItem {
+            if let contents = content.attachments {
+                for (index, attachment) in (contents).enumerated() {
+                    if attachment.hasItemConformingToTypeIdentifier(urlContentType) {
+                        handleUrl(content: content, attachment: attachment, index: index)
+                    }
+                }
+            }
+        }
+    }
+
+    override func didSelectPost() {
+        print("didSelectPost");
+    }
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
+
+
+    private func handleUrl (content: NSExtensionItem, attachment: NSItemProvider, index: Int) {
+        attachment.loadItem(forTypeIdentifier: urlContentType, options: nil) { [weak self] data, error in
+
+            if error == nil, let item = data as? URL, let this = self {
+
+                this.sharedText.append(item.absoluteString)
+
+                // If this is the last item, save imagesData in userDefaults and redirect to host app
+                if index == (content.attachments?.count)! - 1 {
+                    let userDefaults = UserDefaults(suiteName: this.appGroupId)
+                    userDefaults?.set(this.sharedText, forKey: this.sharedKey)
+                    userDefaults?.synchronize()
+                    this.redirectToHostApp(type: .text)
+                }
+
+            } else {
+                self?.dismissWithError()
+            }
+        }
+    }
+
+    private func dismissWithError() {
+        print("[ERROR] Error loading data!")
+        let alert = UIAlertController(title: "Error", message: "Error loading data", preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "Error", style: .cancel) { _ in
+            self.dismiss(animated: true, completion: nil)
+        }
+
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    private func redirectToHostApp(type: RedirectType) {
+        // ids may not loaded yet so we need loadIds here too
+        loadIds();
+        let url = URL(string: "ShareMedia-\(hostAppBundleIdentifier)://dataUrl=\(sharedKey)#\(type)")
+        var responder = self as UIResponder?
+        let selectorOpenURL = sel_registerName("openURL:")
+
+        while (responder != nil) {
+            if (responder?.responds(to: selectorOpenURL))! {
+                let _ = responder?.perform(selectorOpenURL, with: url)
+            }
+            responder = responder!.next
+        }
+        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    }
+
+    enum RedirectType {
+        case media
+        case text
+        case file
+    }
+
+}
+
+extension Array {
+    subscript (safe index: UInt) -> Element? {
+        return Int(index) < count ? self[Int(index)] : nil
+    }
+}
